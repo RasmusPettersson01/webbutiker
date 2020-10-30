a080ff0b2790e520e2965a9376aa9441e2ec06ba
RasmusPettersson01 <rasse200199@gmail.com>
2020-10-30 13:17:08 +0100

    Manifestet

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
-/*
+window.addEventListener("load", () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js");
+  }
+
+  /*
 Copyright 2015, 2019, 2020 Google LLC. All Rights Reserved.
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
